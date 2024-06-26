6f77e7404b443c1ee7ad09facda8c2aebf4ea8c2
Aleks M <aleksandarmaksimovic257@gmail.com>
2024-06-26 23:49:53 +0200

    new approach

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
-const exOverview = document.getElementById('exerciseOverview');
-let newExerciseHTML; 
-function Create_new_exercise(){
-    newExerciseHTML = 
-    `
-    <dialog open>
-    <div id=newExercise>
-    <form>
-    <label for="exerciseFor">Exercise:</label>
-    <input type="text" id="exerciseFor">
-    <label for="weightFor">Weight:</label>
-    <input type="number" id="weightFor">
-    <label for="repetitionsFor">Reps:</label>
-    <input type="number" id="repetitionsFor">
-    <Button onclick = "saveExcercise()"> save </Button> 
-    </form>
-    </dialog>
-    `;
-    exOverview.innerHTML += newExerciseHTML;
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('#add');
+const save = document.querySelector('#save');
+
+openModal.addEventListener('click', () => {
+
+    modal.show();
+
+})
+
+save.addEventListener('click', () => {
+    const exercise = document.getElementById("exerciseFor").value;
+    const weight =  document.getElementById("weightFor").value; 
+    const reps =  document.getElementById("repetitionsFor").value; 
+    const overview = document.getElementById("exerciseOverview");
     
-}
-
-function saveExcercise(){
-
-    newExerciseHTML = 
+    const exerciseHtml = 
     `
-
-    <form>
-    <label for="exerciseFor">Exercise:</label>
-    <label> "document.getElementById('exerciseFor').value" </label> 
-    <label for="weightFor">Weight:</label>
-    <input type="number" id="weightFor">
-    <label for="repetitionsFor">Reps:</label>
-    <input type="number" id="repetitionsFor">
-    <Button onclick = "saveExcercise()"> save </Button> 
-    </form>
-
-    `
-    exOverview.innerHTML += newExerciseHTML;
-}
+    <p> Exercise: ${exercise} Weight: ${weight} Reps: ${reps}
+    `;
+    
+    overview.innerHTML += exerciseHtml; 
+    modal.close();
+})
 
