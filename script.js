9be0d5bb5350da0cf87f65ea6701a13d2fe696be
Aleks M <aleksandarmaksimovic257@gmail.com>
2024-07-29 11:59:00 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const modal = document.querySelector('#modal');
+const modal = document.getElementById('modal');
 const openModal = document.querySelector('#add');
 const save = document.querySelector('#save');
 
 openModal.addEventListener('click', () => {
 
+    console.log("hello");
     modal.show();
 
 })
@@ -12,14 +13,14 @@ save.addEventListener('click', () => {
     const exercise = document.getElementById("exerciseFor").value;
     const weight =  document.getElementById("weightFor").value; 
     const reps =  document.getElementById("repetitionsFor").value; 
-    const overview = document.getElementById("exerciseOverview");
-    
+    const overview = document.getElementById("overview");
+    modal.close();
     const exerciseHtml = 
     `
     <p> Exercise: ${exercise} Weight: ${weight} Reps: ${reps}
     `;
     
     overview.innerHTML += exerciseHtml; 
-    modal.close();
+    
 })
 
